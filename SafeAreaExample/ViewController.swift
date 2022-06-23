ca6c4ed218c097971847ffe815eae0b0ca79be98
Cuongvx <cuongvx@mobio.io>
2022-06-23 21:18:05 +0700

    Initial Commit

---
+++ SafeAreaExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeAreaExample
+//
+//  Created by sun on 23/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
